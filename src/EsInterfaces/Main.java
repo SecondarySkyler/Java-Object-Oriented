44b4cd4c71cc6f7f86457d4d341b308638762e98
Cristian Murtas <skylerhunter6@gmail.com>
2020-12-07 10:58:07 +0100

    Added second part of the exercise + tests

--- src/EsInterfaces/Main.java
+++ src/EsInterfaces/Main.java
@@ -1,15 +1,28 @@
 package EsInterfaces;
 
+import EsInterfaces.Eccezioni.EmptyIntSetException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        int[] a = {2, 6, 10, 1, 4};
-        UnorderedIntSet set = new UnorderedIntSet(a);
-        set.insert(5);
-        set.remove(6);
-        boolean is = set.isIn(5);
-        boolean is2 = set.isIn(6);
-        int dim = set.size();
-        System.out.println(dim);
+    public static void main(String[] args) throws EmptyIntSetException {
+        int[] a = {11, 5, 6};
+        IntSet set = new UnorderedIntSet(a);
+//        set.insert(5);
+//        set.remove(6);
+//        boolean is = set.isIn(5);
+//        boolean is2 = set.isIn(6);
+//        int dim = set.size();
+//        System.out.println(dim);
+
+        OrderedIntSet ois = new OrderedIntSet();
+        ois.insert(5);
+        ois.insert(6);
+        ois.insert(11);
+//        System.out.println(ois.remove(5));
+        System.out.println(ois.max());
+        System.out.println(ois.isIn(11));
+        System.out.println(ois.size());
+        System.out.println(ois.sameValues(set));
+
     }
 }
---
+++ src/EsInterfaces/OrderedIntSet.java
@@ -0,0 +1,67 @@
+package EsInterfaces;
+
+import EsInterfaces.Eccezioni.EmptyIntSetException;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class OrderedIntSet implements IntSet{
+    TreeSet<Integer> elements;
+
+    public OrderedIntSet() {
+        this.elements = new TreeSet<Integer>();
+    }
+
+    @Override
+    public void insert(int x) {
+        this.elements.add(x);
+    }
+
+    @Override
+    public boolean remove(int x) {
+        return this.elements.remove(x);
+    }
+
+    @Override
+    public boolean isIn(int x) {
+        return this.elements.contains(x);
+    }
+
+    @Override
+    public int size() {
+        return this.elements.size();
+    }
+
+    @Override
+    public int max() throws EmptyIntSetException {
+        if (this.elements == null)
+            throw new EmptyIntSetException();
+        return this.elements.last();
+    }
+
+    @Override
+    public int choose() throws EmptyIntSetException {
+        if (this.elements == null)
+            throw new EmptyIntSetException();
+
+        int random = (int) Math.random() * this.elements.size();
+       return this.elements.subSet(random, true, random, true).floor(random);
+    }
+
+    @Override
+    public boolean sameValues(IntSet s2) {
+        boolean res = true;
+        Iterator<Integer> it = this.iterator();
+        while (it.hasNext()) {
+            Integer next = it.next();
+            if (!s2.isIn(next))
+                res = false;
+        }
+        return res;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return this.elements.iterator();
+    }
+}
