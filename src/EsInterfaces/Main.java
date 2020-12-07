105b27d3d887c1ae513cec3fee291b5cafc8b7d2
Cristian Murtas <skylerhunter6@gmail.com>
2020-12-07 11:09:09 +0100

    Added method subSet

--- src/EsInterfaces/Main.java
+++ src/EsInterfaces/Main.java
@@ -5,8 +5,10 @@ import EsInterfaces.Eccezioni.EmptyIntSetException;
 public class Main {
 
     public static void main(String[] args) throws EmptyIntSetException {
-        int[] a = {11, 5, 6};
+        int[] a = {2, 48, 3, 11, 4, 56, 6, 10, 5};
+        int[] b = {11, 5, 6};
         IntSet set = new UnorderedIntSet(a);
+        IntSet set2 = new UnorderedIntSet(b);
 //        set.insert(5);
 //        set.remove(6);
 //        boolean is = set.isIn(5);
@@ -22,7 +24,8 @@ public class Main {
         System.out.println(ois.max());
         System.out.println(ois.isIn(11));
         System.out.println(ois.size());
-        System.out.println(ois.sameValues(set));
+        System.out.println(ois.sameValues(set2));
+        System.out.println(ois.subSet(set));
 
     }
 }
--- src/EsInterfaces/OrderedIntSet.java
+++ src/EsInterfaces/OrderedIntSet.java
@@ -51,10 +51,31 @@ public class OrderedIntSet implements IntSet{
     @Override
     public boolean sameValues(IntSet s2) {
         boolean res = true;
+        if (this.elements.size() != s2.size())
+            res = false;
+
         Iterator<Integer> it = this.iterator();
         while (it.hasNext()) {
             Integer next = it.next();
             if (!s2.isIn(next))
+                res = false;
+        }
+        return res;
+    }
+
+    /**
+     * check if this is a subset of s
+     * @param s
+     * @return true if this is contained in s
+     */
+    public boolean subSet(IntSet s) {
+        boolean res = true;
+        if ((s.size() < this.elements.size()))
+            res = false;
+        Iterator<Integer> it = this.iterator();
+        while (it.hasNext()) {
+            Integer next = it.next();
+            if (!s.isIn(next))
                 res = false;
         }
         return res;
