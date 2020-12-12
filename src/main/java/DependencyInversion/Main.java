9f0a5f84ecd8e6f64a9b5943af87c02efa19d333
Cristian Murtas <skylerhunter6@gmail.com>
2020-12-12 15:38:34 +0100

    tested method toJson, works fine

--- src/main/java/DependencyInversion/Main.java
+++ src/main/java/DependencyInversion/Main.java
@@ -1,4 +1,21 @@
 package DependencyInversion;
 
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDate;
+
 public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+        Anagrafica elenco = new Anagrafica();
+        CarrieraUniversitaria c = new CarrieraUniversitaria();
+
+        Studente s1 = new Studente("Francesca", "Rossi", "123456", c);
+        elenco.iscrivi(s1, LocalDate.parse("2020-12-12"));
+        s1.registraVoto(LocalDate.now(), "POO", 25, false);
+
+        JSONizzatore js = JSONizzatore.instance;
+        js.salva(elenco);
+        System.out.print(js.toJson(elenco));
+    }
 }
